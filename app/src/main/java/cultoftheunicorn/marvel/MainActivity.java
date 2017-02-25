85daad50b0daf53fd40fc50bd7301a54dcc353c5
Abhishek Nagekar <abhisheknagekar@gmail.com>
2017-02-26 02:04:04 +0530

    opencv integrated

--- app/src/main/java/cultoftheunicorn/marvel/MainActivity.java
+++ app/src/main/java/cultoftheunicorn/marvel/MainActivity.java
@@ -2,12 +2,28 @@ package cultoftheunicorn.marvel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.engine.OpenCVEngineInterface;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
